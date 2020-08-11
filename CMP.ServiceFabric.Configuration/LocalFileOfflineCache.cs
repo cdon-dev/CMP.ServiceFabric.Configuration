e331f5a63fe5963325bbf9ce5945caf4c5140ba4
Gulfam Abbas <gulfam.abbas@cdon.com>
2020-08-11 09:56:49 +0200

    Enable OfflineCache

---
+++ CMP.ServiceFabric.Configuration/LocalFileOfflineCache.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+
+namespace CMP.ServiceFabric.Configuration
+{
+    public class LocalFileOfflineCache : IOfflineCache
+    {
+        private readonly string _cacheFilePath;
+
+        public LocalFileOfflineCache(string filePath)
+        {
+            _cacheFilePath = Path.Combine(filePath, "offlineCache.json");
+        }
+
+        public void Export(AzureAppConfigurationOptions options, string data)
+        {
+            File.WriteAllText(_cacheFilePath, data);
+        }
+
+        public string Import(AzureAppConfigurationOptions options)
+        {
+            return File.ReadAllText(_cacheFilePath);
+        }
+    }
+}
